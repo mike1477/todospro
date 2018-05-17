0fad6214b4ba76ae51b82b723ed4bb602b3a76f6
Mike <mlh1477@gmail.com>
2018-05-17 08:18:58 -0400

    Create header and user components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    EditUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
